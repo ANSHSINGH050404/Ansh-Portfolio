3e703bf965f871405a025af2f668d54c0199eb50
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2025-07-01 13:24:23 +0530

    Add initial layout and footer components for the portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import StarsCanvas from "@/components/main/StarBackground";
+
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
 
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#030014] overflow-scroll overflow-x-hidden` }
       >
-        <StarsCanvas/>
+        
         <Navbar/>
         {children}
         <Footer/>
